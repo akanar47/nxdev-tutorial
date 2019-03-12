7750f75a81d6d98bdf25b8b254cc09af6de4c2a6
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-03-12 15:47:48 +0100

    Add common interface

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,9 +1,6 @@
+import { Todo } from '@nxtutorial/data';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Todo {
-  title: string;
-}
 
 @Component({
   selector: 'nxtutorial-root',
